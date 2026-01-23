b878a60738deaad912acb96bec5e01f5ec83397d
Charles-indigo21 <charles.verdadero@indigo21.com>
2026-01-23 19:44:53 +0800

    Update AccessorySeeder.php

--- database/seeders/AccessorySeeder.php
+++ database/seeders/AccessorySeeder.php
@@ -24,69 +24,9 @@ class AccessorySeeder extends Seeder
                 'created_by'    => '1'
 
             ],
-            [
-                'name'          => 'Granite Heart - S/A Dark Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Heart - Blue Pearl',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Heart - Karin Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Heart - Balmoral Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Heart - Imperial Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Heart - Tropical Green',
-                'created_by'    => '1'
-
-            ],
 
             [
                 'name'          => 'Granite Book',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Book - S/A Dark Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Book - Blue Pearl',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Book - Karin Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Book - Balmoral Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Book - Imperial Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Book - Tropical Green',
                 'created_by'    => '1'
 
             ],
@@ -96,69 +36,9 @@ class AccessorySeeder extends Seeder
                 'created_by'    => '1'
 
             ],
-            [
-                'name'          => 'Granite Vase - S/A Dark Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Vase - Blue Pearl',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Vase - Karin Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Vase - Balmoral Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Vase - Imperial Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Vase - Tropical Green',
-                'created_by'    => '1'
-
-            ],
 
             [
                 'name'          => 'Marble Heart',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Heart - S/A Dark Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Heart - Blue Pearl',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Heart - Karin Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Heart - Balmoral Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Heart - Imperial Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Heart - Tropical Green',
                 'created_by'    => '1'
 
             ],
@@ -168,69 +48,9 @@ class AccessorySeeder extends Seeder
                 'created_by'    => '1'
 
             ],
-            [
-                'name'          => 'Marble Book - S/A Dark Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Book - Blue Pearl',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Book - Karin Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Book - Balmoral Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Book - Imperial Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Book - Tropical Green',
-                'created_by'    => '1'
-
-            ],
 
             [
                 'name'          => 'Marble Vase',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Vase - S/A Dark Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Vase - Blue Pearl',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Vase - Karin Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Vase - Balmoral Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Vase - Imperial Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Marble Vase - Tropical Green',
                 'created_by'    => '1'
 
             ],
@@ -240,73 +60,12 @@ class AccessorySeeder extends Seeder
                 'created_by'    => '1'
 
             ],
-            [
-                'name'          => 'Granite Page Book - S/A Dark Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Book - Blue Pearl',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Book - Karin Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Book - Balmoral Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Book - Imperial Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Book - Tropical Green',
-                'created_by'    => '1'
-
-            ],
 
             [
                 'name'          => 'Granite Page Heart',
                 'created_by'    => '1'
 
-            ],
-            [
-                'name'          => 'Granite Page Heart - S/A Dark Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Heart - Blue Pearl',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Heart - Karin Grey',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Heart - Balmoral Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Heart - Imperial Red',
-                'created_by'    => '1'
-
-            ],
-            [
-                'name'          => 'Granite Page Heart - Tropical Green',
-                'created_by'    => '1'
-
-            ],
-            
+            ]
 
         ];
         foreach ($values as $key => $value) {
