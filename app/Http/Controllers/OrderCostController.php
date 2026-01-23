6b761ac3394398feb3df618f103da290e7836e12
Charles-indigo21 <charles.verdadero@indigo21.com>
2026-01-23 19:46:27 +0800

    Create OrderCostController.php

---
+++ app/Http/Controllers/OrderCostController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderCost;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class OrderCostController extends Controller
+{
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OrderCost $orderCost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OrderCost $orderCost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OrderCost $orderCost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OrderCost $orderCost)
+    {
+        //
+    }
+}
