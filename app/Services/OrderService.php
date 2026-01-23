3a96fc5a34413d4485efd8473b19035a32f0f286
Charles-indigo21 <charles.verdadero@indigo21.com>
2026-01-23 19:45:47 +0800

    Create OrderService.php

---
+++ app/Services/OrderService.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Services;
+use App\Models\Order;
+use App\Models\OrderCost;
+use App\Models\OrderNote;
+use App\Services\CustomerService;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+
+class OrderService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct( protected CustomerService $customerService)
+    {}
+
+    public function order_upsert($request, $id = false){
+        // $customer_id = $request->customer_id;
+        $date_of_death = $request->date_of_death;
+        $grave_number_checked = $request->grave_no_checked;
+        $fixing_date = $request?->fixed_required_by ?? null;
+        $customer_id = $this->customerService->form_action($request, $request->customer_id);
+        
+        if($date_of_death){
+            $date_of_death = str_replace(['AM', 'PM'], '', $date_of_death);
+            $date_of_death = trim($date_of_death).' 12:00 '.(str_contains($request->date_of_death, 'PM') ? 'PM' : 'AM'); 
+        }
+
+        if($fixing_date != null){
+            $fixing_date = str_replace(" ", ' 01, ', $request?->fixed_required_by);
+        }
+
+
+        $data = !$id ? new Order() : Order::findOrFail($id);
+
+        $data->order_type_id = $request->order_type_id;
+        $data->location_id = $request->location_id;
+
+        if($id){
+            $data->invoice_no = $request->invoice_no;
+            $data->invoice_date = Carbon::parse($request->invoice_date)->format('Y-m-d');
+        }
+
+        $data->customer_id = $customer_id;
+        $data->date_of_death = $date_of_death ? Carbon::parse($date_of_death)->format('Y-m-d h:i:s') : null;
+        $data->deceased_name = $request->deceased_name;
+        $data->consecration_date = $request->consecration_date ? Carbon::parse($request->consecration_date)->format('Y-m-d') : null;
+        
+        $data->cemetery_id = $request->cemetery_id;
+
+        if(isset($request->fixed_date)){
+            switch ($request->fixed_date) {
+                case 'is_tba':
+                    $data->is_tba = 1;
+                    break;
+                case 'is_approx':
+                    $data->is_approx = 1;
+                    break;
+                
+                default:
+                    $data->is_asap = 1;
+                    break;
+            }
+        }
+
+        // $data->fixing_date = Carbon::parse($request?->fixed_required_by)->format('Y-m-d') ?? null;
+        $data->fixing_date = Carbon::parse($fixing_date)->format('Y-m-d') ?? null;
+       
+
+
+        $data->burial_society_organization_id = $request->burial_society_organization_id;
+        $data->grave_number = $request->grave_no;
+        $data->grave_number_checked = Carbon::parse($request?->grave_no_checked)->format('Y-m-d') ?? null;
+        $data->grave_space_id = $request->grave_space_id;
+        $data->design_headstone = $request->design_headstone;
+        $data->material = $request->material;
+        $data->material_colour = $request->material_colour;
+        $data->size = $request->size;
+        $data->base_ledger = $request->base_ledger;
+        $data->letter_type = $request->letter_type;
+        $data->accessory = $request->accessory;
+        $data->accessory_colour = $request->accessory_colour;
+        $data->kerb_riser = $request->kerb_riser;
+        $data->issue = $request->issue;
+        $data->special_instruction = $request->special_instruction;
+        $data->customer_notes = $request->customer_note;
+        $data->additional_notes = $request->additional_note;
+
+        $data->{$id ? "updated_by" : "created_by"} = Auth::id();
+        $message = "Detect issues in the order request";
+        $result = $data->save();
+        
+        if($result){
+            $order_id = $data->id;
+            $request["order_id"] = $order_id;
+            $result_cost  = self::order_cost_upsert($request, $id);
+            $result_note  = self::order_note_upsert($request, $id);
+            $message = $id ? "Order No. $id has been successfully updated" : "New order successfully created.";
+            if(!$result_cost) $message = "Detect issues in the Order Cost request";
+            if(!$result_note) $message = "Detect issues in the Order Note request";
+        }
+        // return $result ? $result->id() : dd("Error on: Order Related");
+        return [
+                "success" => $result ? true : false,
+                "message" => $message
+        ];
+    }
+    
+    public function order_cost_upsert($request, $id = false){
+
+        $data = !$id ? new OrderCost() : OrderCost::findOrFail($id);
+
+        $data->order_id = $request->order_id;
+        $data->price_description = $request->price_description;
+        $data->price_description_amount = $request->price_description_amount;
+        $data->letter_count = $request->letter_count;
+        $data->letter_amount = $request->letter_amount;
+        $data->price_description_1 = $request->price_description_1;
+        $data->price_description_amount_1 = $request->price_description_amount_1;
+        $data->price_description_2 = $request->price_description_2;
+        $data->price_description_amount_2 = $request->price_description_amount_2;
+        $data->price_description_3 = $request->price_description_3;
+        $data->price_description_amount_3 = $request->price_description_amount_3;
+        $data->discount_description = $request->discount_description;
+        $data->discount_amount = $request->discount_amount;
+        $data->total = $request->total;
+        $data->cemetery_fee_description_1 = $request->cemetery_fee_description_1;
+        $data->cemetery_fee_amount_1 = $request->cemetery_fee_amount_1;
+        $data->cemetery_fee_description_2 = $request->cemetery_fee_description_2;
+        $data->cemetery_fee_amount_2 = $request->cemetery_fee_amount_2;
+        $data->grand_total = $request->grand_total;
+        $data->deposit_description = $request->deposit_description;
+        $data->deposit_amount = $request->deposit_amount;
+        $data->amount_received = $request->amount_received;
+        $data->balance = $request->balance;
+        $data->net_amount = $request->net_amount;
+        $data->vat_rate = $request->vat_rate;
+        $data->vat_amount = $request->vat_amount;
+        $data->zero_rated_fee = $request->zero_rated_fee;
+        $data->adjustment = $request->adjustment;
+        $data->gross_amount = $request->gross_amount;
+        $data->is_cost_analysis_print = $request->is_cost_analysis_print;
+        $data->is_cost_analysis_trade = $request->is_cost_analysis_trade;
+
+        $result = $data->save();
+
+        return $result ? $data->id : false;
+    }
+
+    public function order_note_upsert($request, $id = false){
+        $data = !$id ? new OrderNote() : OrderNote::findOrFail($id);
+
+        $data->order_id = $request->order_id;
+        $data->free_letters = $request->free_letters;
+        $data->is_burial_society_fees_included = $request->is_burial_society_fees_included;
+        $data->is_inscription_complete = $request->is_inscription_complete;
+        $data->is_application_form_sent_to_bs_with_cheque = $request->is_application_form_sent_to_bs_with_cheque;
+        $data->is_application_form_sent_to_bs_without_cheque = $request->is_application_form_sent_to_bs_without_cheque;
+        $data->is_permit_not_required = $request->is_permit_not_required;
+        $data->is_insurance = $request->is_insurance;
+        $data->is_insurance_services = $request->is_insurance_services;
+        $data->is_washdown_discussed = $request->is_washdown_discussed;
+        $data->is_paid_by_bacs = $request->is_paid_by_bacs;
+        $data->is_full_inscription_received = $request->is_full_inscription_received;
+        $data->is_sent_to_burial_society = $request->is_sent_to_burial_society;
+        $data->is_received_from_burial_society = $request->is_received_from_burial_society;
+        $data->is_order_complete = $request->is_ordered_complete;
+        $data->inscription_sent_to_design_team_for_printout = Carbon::parse($request?->inscription_sent_to_design_team_for_printout)->format('Y-m-d') ?? null;
+        $data->inscription_sent_to_gary_for_printout = Carbon::parse($request?->inscription_sent_to_gary_for_printout)->format('Y-m-d') ?? null;
+        $data->received_back_from_design_team = Carbon::parse($request?->received_back_from_design_team)->format('Y-m-d') ?? null;
+        $data->sent_to_customer = Carbon::parse($request?->sent_to_customer)->format('Y-m-d') ?? null;
+        $data->back_to_design_team_for_further_alterations = Carbon::parse($request?->back_to_design_team_for_further_alterations)->format('Y-m-d') ?? null;
+        $data->masonart_printout_approved = Carbon::parse($request?->masonart_printout_approved)->format('Y-m-d') ?? null;
+        $data->approved_by_burial_society = Carbon::parse($request?->approved_by_burial_society)->format('Y-m-d') ?? null;
+        
+        $result = $data->save();
+
+        return $result ? $data->id : false;              
+    }
+    
+
+}
